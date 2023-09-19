361a774faf015bf7c815d5ed7f3af40a2167ff0a
Data Maven <46922974+Jenesequoi@users.noreply.github.com>
2023-09-19 13:16:44 +0300

    Python classroom exercises
    Countdown using while loop

---
+++ Countdown problems.py
@@ -0,0 +1,42 @@
+#countdown problems
+"""1. Using a while loop, print numbers from 50 to 0."""
+
+count= 50
+while count!= 0:
+    count-=1
+    print(count)
+
+"""2. Using a while loop, print odd numbers from 100 to 1."""
+
+count= 100
+while count!= 0:
+    odds= count%2== 0
+    count-= 1
+    if odds== 1:
+        print(count)
+
+"""3. Using a while loop, print even numbers from 200 to 100."""
+
+count= 200
+while count!= 100:
+    evens= count%2== 0
+    count-=1
+    if evens== 0:
+        print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
