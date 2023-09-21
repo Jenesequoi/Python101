4909bbdbeed6077cc784a8337ba27f57d9c006be
Data Maven <46922974+Jenesequoi@users.noreply.github.com>
2023-09-22 02:06:03 +0300

    Added exercise 2: Loops and functions
    Part 1

--- Python exercises for data science by tomas beuzen.py
+++ Python exercises for data science by tomas beuzen.py
@@ -1,5 +1,5 @@
 #Python exercises for data science by tomas beuzen
-##Exercise 1
+##Exercise 1 Basics
 
 """1. Arithmetics"""
 i= 5**5
@@ -67,6 +67,40 @@ elif language == 'R':
 else: print('What is {}. format(language)')
 
 
+---------------------------------------------------------------------------------------------------------------------------------------------------
+
+#Exercise 2 Loops and functions
+
+"""1. Create a function website() that grabs the website domain from a url string."""
+
+def website(url):
+    """Function grabs the website domain from url"""
+    domain= url.split('.')[1]
+    return domain
+
+my_web= website('www.google.com')
+my_web
+
+"""2. Create a function divisible(a, b) that accepts two integers (a and b) and returns True if a is divisble by b without a remainder."""
+def divisible(a, b):
+    return (a%b== 0)
+
+null= divisible(6,3)
+null
+
+"""3. Use list comprehension to square every number in the following list of numbers."""
+l= [1,2,3,4,5,6,7,8,9]
+
+for i in l:
+    print(i**2)
+
+"""4. For the following list of names, write a list comprehension that creates a list of only words that start with a capital letter."""
+names= ['Steve Irwin', 'koala', 'kangaroo', 'Australia', 'Sydney', 'desert']
+
+for name in names:
+    for i in name:
+        if i.isupper():
+            print(name)
 
 
 
@@ -75,4 +109,60 @@ else: print('What is {}. format(language)')
 
 
 
+
+
+
+
+
+
+def sum_and_product(x,y):
+    return(x+y, x*y)
+sum_and_product(5,6)
+
+s,_= sum_and_product(5,6)
+s
+_
+
+names= ['milad', 'tom', 'tiffany']
+name= 'tom'
+name[::-1]
+
+names_backwards= list()
+
+names_backwards.append(names[0]+names[0][::-1])
+names_backwards.append(names[1]+names[1][::-1])
+names_backwards.append(names[2]+names[2][::-1])
+
+names_backwards
+
+names_backwards= list()
+
+for name in names:
+    names_backwards.append(name+ name[::-1])
+
+names_backwards
+
+def make_palindrome (names):
+    names_backwards= list()
+    for name in names:
+        names_backwards.append(name + name[::-1])
+    
+    return names_backwards
+
+
+names1= ['apples', 'oranges', 'bananas']
+
+make_palindrome(names1)
+
+def gen():
+    for n in range(10):
+        yield(n, n**2)
+
+g=gen()
+print(next(g))
+
+
+x= ([1,2,3], 5)
+x[0][1]= 4
+x
 
