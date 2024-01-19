6a95f35e028c39fdb7fc0fcc8ce8b9f88c2174f2
Data Maven <46922974+Jenesequoi@users.noreply.github.com>
2024-01-19 11:56:23 +0300

    An intro to matplotlib

---
+++ visualization with python.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+plt.plot([7, 8, 9], [1, 3, 12])
+plt.xlabel('x label')
+plt.ylabel('y axis')
+plt.show()
+
+# import matplotlib.pyplot as plt
+# plt.plot([1, 2, 3], [5, 6, 4])
+# plt.show()
